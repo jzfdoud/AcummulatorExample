ce9ec87788b802c97c5b95b3fed4c9641bd86d55
Jada <jada.fagan@gmail.com>
2020-06-17 16:31:09 -0400

    High/Low example
    * tried to find the high and low number from a random set of numbers
    * combined int, var, foreach, and if

--- Accumulator2/Program.cs
+++ Accumulator2/Program.cs
@@ -7,12 +7,35 @@ namespace Accumulator2
         static void Main(string[] args)
         {
      
-            var ans = 0;
-            for (var nbr = 0; nbr <= 50; nbr++) { 
-            if (nbr % 5 == 0 || nbr % 7 == 0)
-                    ans += nbr;
+            //var ans = 0;
+            //for (var nbr = 0; nbr <= 50; nbr++) { 
+            //if (nbr % 5 == 0 || nbr % 7 == 0)
+            //        ans += nbr;
+            //}
+            //Console.WriteLine($"This is the sum of numbers 1 to 50 divisable by 5 or 7 : {ans}");
+
+            int[] ints = {
+                754, 233, 509, 792, 700, 596, 833, 658, 998, 742,
+                187, 754, 308, 914, 489, 867, 717, 586, 929, 467,
+                460, 241, 770, 324, 599, 259, 120, 800, 336, 609,
+                690, 134, 598, 249, 282, 574, 334, 956, 659, 214,
+                435, 643, 809, 874, 906, 620, 328, 369, 426, 561
+            };
+            var highest = 0;
+            var lowest = 1000;
+            foreach(var i in ints)
+            {
+                if(i < lowest)
+                {
+                    lowest = i;
+                }
+                if(i > highest)
+                {
+                    highest = i;
+                }
             }
-            Console.WriteLine($"This is the sum of numbers 1 to 50 divisable by 5 or 7 : {ans}");
+            Console.WriteLine($" HIghest is {highest}; Lowest is {lowest}");
+
         }
     }
 }
