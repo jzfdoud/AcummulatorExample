d7219d28cf6bb84a51e30a69f1b9e8493707ae96
Jada <jada.fagan@gmail.com>
2020-06-17 16:05:27 -0400

    Add project files.

---
+++ Accumulator2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Accumulator2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+     
+            var ans = 0;
+            //var nbr = Convert.ToInt32(ans);
+            for (var nbr = 0; nbr <= 50; nbr++) { 
+            if (nbr % 5 == 0 || nbr % 7 == 0)
+                    ans += nbr;
+            }
+            Console.WriteLine($"This is the sum of numbers 1 to 50 divisable by 5 or 7 : {ans}");
+        }
+    }
+}
